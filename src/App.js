f08d253c14b4f303bdc30169a7fbec9989aa36c3
ikran-04 <88615778+ikran-04@users.noreply.github.com>
2022-05-19 23:44:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,50 +1,36 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
-import "./App.css"
+import React from "react";
+import {
+  BrowserRouter,
+  Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+// import {browserHistory} from "connect-history-api-fallback"
+import HomePage from "./Pages/home";
+import Navbar from "./components/Navbar";
+import Products from "./Pages/products";
+// import Products from "./components/Products";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
+function App() {
+  return (
+<>
 
-  handleClick = api => e => {
-    e.preventDefault()
+      <Navbar />
 
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
+   
+     <BrowserRouter>
+     <Routes >
+       <Route  exact path="/"  element={<HomePage/>} />
+       <Route path="expenses" element={ <Products/> } />
+       </Routes>
+       </BrowserRouter>
+   
 
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
+</>
+  
+  
+   
+  );
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
-    )
-  }
-}
-
-export default App
+export default App;
